a0bf31906b7af39ea5037b79056a6fba0268dd26
Brent Colson <brentcolson@Mac-Mini.home>
2016-04-17 11:27:52 -0400

    First Commit

---
+++ TechRun.js
@@ -0,0 +1,12 @@
+//Tech Run
+
+
+var hrr16 = {
+	name: 'Daniel Ramos',
+  name: 'Raymond To',
+  name: "Keeley Nakamoto",
+  name: "Brent Colson",
+  name: "Leo Vergara",
+  name: "Tyrus Brenc",
+  name: "Sangboak Lee"
+ }
